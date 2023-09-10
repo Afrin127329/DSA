672954f3b70d260e6353cd9c43ea820d0e1ec15b
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-10 17:14:16 +0600

    update on bubbleSort

--- sorting-algo/BS-numbers.js
+++ sorting-algo/BS-numbers.js
@@ -1,11 +1,14 @@
 /**
  * Problem: Sort an array of numbers using the Bubble Sort algorithm.
- * Soln: 
+ * Soln:  
  * 1. Iterate over the array elements from the beginning.
  * 2. For each iteration, compare adjacent elements and swap them if they are in the wrong order.
  * 3. Continue iterating and swapping until no more swaps are needed in a pass.
  * 4. Repeat steps 1-3 for the required number of passes (equal to the length of the array).
  * 5. The array is sorted when no swaps are made in a pass.
+ * 
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
  */
 
 function bubbleSort(array) {
@@ -35,5 +38,4 @@ const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
 bubbleSort(unsortedArray);
 console.log(unsortedArray);
 
-// Time complexity: O(n^2)
-// Space complexity: O(1)
+
